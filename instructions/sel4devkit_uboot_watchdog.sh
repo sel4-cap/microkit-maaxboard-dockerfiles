f6b9699574352ea6c17446b3a7bfcf675163643b
Bill Ellis <bellis@altran.com>
2024-03-05 13:26:01 +0000

    Widen net. Collect first.

--- instructions/sel4devkit_uboot_watchdog.sh
+++ instructions/sel4devkit_uboot_watchdog.sh
@@ -13,6 +13,8 @@ mkdir -p "${BUILD_PATH}"
 mkdir -p "${PACKAGE_PATH}"
 #========================================
 
+# Step 1:
+
 # Acquire maaxboard-uboot.
 cd "${BUILD_PATH}"
 git clone --branch "main" "git@github.com:sel4devkit/maaxboard-uboot.git" maaxboard-uboot
@@ -29,6 +31,19 @@ bash firmware-imx-8.14.bin --auto-accept
 cd "${BUILD_PATH}/maaxboard-uboot"
 ./build-offline.sh
 
+# Step 2:
+# The Watchdog build is not completely successful. However, we only need a few
+# components to assemble our desired U-Boot. We implicitly use some of the
+# build residue from above to permit the Watchdog build. This is not ideal.
+
+# Patch in Watchdog.
+cd "${BUILD_PATH}/maaxboard-uboot"
+echo "CONFIG_IMX_WATCHDOG=y" >> "uboot-imx/configs/maaxboard_defconfig"
+
+# Build.
+cd "${BUILD_PATH}/maaxboard-uboot"
+./build-offline.sh
+
 # Retain.
-mkdir -p "${PACKAGE_PATH}/uboot/sel4devkit_uboot"
-cp -r "${BUILD_PATH}/maaxboard-uboot/flash.bin" "${PACKAGE_PATH}/uboot/sel4devkit_uboot/flash.bin"
+mkdir -p "${PACKAGE_PATH}/uboot/sel4devkit_uboot_watchdog"
+cp -r "${BUILD_PATH}/maaxboard-uboot/flash.bin" "${PACKAGE_PATH}/uboot/sel4devkit_uboot_watchdog/flash.bin"
