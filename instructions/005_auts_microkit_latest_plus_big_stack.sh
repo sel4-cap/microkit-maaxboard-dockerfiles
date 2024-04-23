a5e2da9a3484557028feb56151a7465d78aaf74e
Bill Ellis <bellis@altran.com>
2024-04-23 11:55:28 +0100

    More build instructions.

--- instructions/005_auts_microkit_latest_plus_big_stack.sh
+++ instructions/005_auts_microkit_latest_plus_big_stack.sh
@@ -13,20 +13,30 @@ mkdir -p "${BUILD_PATH}"
 mkdir -p "${PACKAGE_PATH}"
 #========================================
 
+# The prebuilt Microkit filename for SDDF is
+# "microkit-sdk-dev-8e40fe8-linux-x86-64.tar.gz". We observe that commit
+# "8e40fe8c85d7b1b41e8348c4d96bd2aa137eb9d9" is on the "dev" branch of Ivan's
+# forked Microkit. We choose to take latest, as we expect this is the right
+# thing to do as this progresses. We also use Ivan's forked sel4 (at latest,
+# of branch "microkit-dev"), as the dependencies for the branched Microkit
+# cite this.
+
 # Acquire seL4.
 cd "${BUILD_PATH}"
-git clone --branch "microkit" "git@github.com:seL4/seL4.git" sel4
-cd "${BUILD_PATH}/sel4"
-git reset --hard "7008430d4432c71a74b2a1da0afae58f7a8658df"
+git clone --branch "microkit-dev" "git@github.com:Ivan-Velickovic/seL4.git" sel4
 
 # Acquire microkit.
 cd "${BUILD_PATH}"
-git clone --branch "main" "git@github.com:seL4/microkit.git" microkit
+git clone --branch "dev" "git@github.com:Ivan-Velickovic/microkit.git" microkit
 cd "${BUILD_PATH}/microkit"
+
+
+# We amend the stack-hack-patch for Ivan's forked Microkit, which has a
+# different layout. We only patch for aarch64 (our target).
 
 # Increase Stack Patch.
 cd "${BUILD_PATH}/microkit"
-sed -i libmicrokit/src/crt0.s -e 's/0xff0/0x3FFE/g'
+sed -i libmicrokit/src/aarch64/crt0.s -e 's/0xff0/0x3FFE/g'
 sed -i libmicrokit/src/main.c -e 's/_stack\[4096\]/_stack[16384]/g'
 git diff
 
@@ -37,14 +47,12 @@ python -m venv "pyenv"
 "${BUILD_PATH}/pyenv/bin/pip" install -r "${BUILD_PATH}/microkit/requirements.txt"
 
 # By default, microkit builds for all targets, which imposes greater compiler
-# dependencies than we wish. Currently, this is awkward to deactivate (on this
-# variant of Microkit), and appears to function for us. In due course,
-# probally add: --filter-boards "maaxboard"
+# dependencies than we wish. Pick maaxboard, only.
 
 # Build.
 cd "${BUILD_PATH}/microkit"
 export PATH="/util/sel4_foundation_arm_toolchain_baseline/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf/bin:${PATH}"
-"${BUILD_PATH}/pyenv/bin/python" build_sdk.py --sel4="${BUILD_PATH}/sel4" 
+"${BUILD_PATH}/pyenv/bin/python" build_sdk.py --sel4="${BUILD_PATH}/sel4" --filter-boards "maaxboard"
 
 # Ensure the built binary has execute for all.
 # The sel4 build scripts do not ensure this, when building as root.
@@ -56,5 +64,5 @@ mkdir -p "built"
 make BUILD_DIR="built" MICROKIT_SDK="${BUILD_PATH}/microkit/release/microkit-sdk-1.2.6" MICROKIT_BOARD="maaxboard" MICROKIT_CONFIG="debug"
 
 # Retain built release.
-mkdir -p "${PACKAGE_PATH}/microkit/sel4_foundation_microkit_latest_plus_big_stack"
-cp -r "${BUILD_PATH}/microkit/release" "${PACKAGE_PATH}/microkit/sel4_foundation_microkit_latest_plus_big_stack/release"
+mkdir -p "${PACKAGE_PATH}/microkit/auts_microkit_latest_plus_big_stack"
+cp -r "${BUILD_PATH}/microkit/release" "${PACKAGE_PATH}/microkit/auts_microkit_latest_plus_big_stack/release"
